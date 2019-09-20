4a8ed7404b5768471d5ada3b3cdedb51d40fd1a0
GD3 <GD3@DESKTOP-0JLRCGV>
2019-09-20 16:03:16 +0900

    employees테이블과 departments테이블의 내용을 담은 클래스 생성 후 getter/setter

---
+++ employees/src/vo/Department.java
@@ -0,0 +1,24 @@
+package vo;
+
+public class Department {
+	private String deptNo;
+	private String deptName;
+	
+	public String getDeptNo() {
+		return deptNo;
+	}
+	public void setDeptNo(String deptNo) {
+		this.deptNo = deptNo;
+	}
+	public String getDeptName() {
+		return deptName;
+	}
+	public void setDeptName(String deptName) {
+		this.deptName = deptName;
+	}
+	
+	@Override
+	public String toString() {
+		return "Department [deptNo=" + deptNo + ", deptName=" + deptName + "]";
+	}
+}
