5212f5c65db33cb684ae9b6e65cc12873dd68b55
GD3 <GD3@DESKTOP-0JLRCGV>
2019-09-25 16:54:46 +0900

    employees테이블의 리스트를 페이징 까지 되도록 출력 매개변수 currentPage와 rowPerPage를 가지는 메서드 생성 및 lastPage 설정을 위해 매개변수 rowPerPage를 가지는 메서드를 생성하여 lastPage를 설정함 10,20,30 등 ~개씩 보기를 폼태그로 보여주도록 구현함

---
+++ employees/src/controller/GetEmployeesListByPageServlet.java
@@ -0,0 +1,60 @@
+package controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.EmployeesDao;
+import vo.Employees;
+
+
+@WebServlet("/employees/getEmployeesListByPage")
+public class GetEmployeesListByPageServlet extends HttpServlet {
+	private EmployeesDao employeesDao;
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("--------------사원리스트 페이징--------------");
+		// currentPage , rowPerPage받음
+		int currentPage = 1;
+		if(request.getParameter("currentPage") != null) {
+			currentPage = Integer.parseInt(request.getParameter("currentPage"));
+		}
+		System.out.println("currentPage : " + currentPage);
+		
+		int rowPerPage = 10;
+		if(request.getParameter("rowPerPage") != null) {
+			rowPerPage = Integer.parseInt(request.getParameter("rowPerPage"));
+		}
+		
+		System.out.println("rowPerPage : " + rowPerPage);
+		
+		// 페이징 작업 ==> 마지막 페이지 설정
+		int lastPage = employeesDao.selectLastPage(rowPerPage);
+		System.out.println("lastPage : " + lastPage);
+		
+		// 리스트 출력
+		List<Employees> list = employeesDao.selectEmployeesListByPage(currentPage, rowPerPage);
+		
+		// request에 내용을 저장해서 보내줌
+		request.setAttribute("currentPage", currentPage);
+		request.setAttribute("rowPerPage", rowPerPage);
+		request.setAttribute("lastPage", lastPage);
+		request.setAttribute("list", list);
+		
+		// 포워딩
+		request.getRequestDispatcher("/WEB-INF/views/employees/getEmployeesListByPage.jsp").forward(request, response);
+	}
+}
+
+
+
+
+
+
+
+
+
--- employees/src/model/EmployeesDao.java
+++ employees/src/model/EmployeesDao.java
@@ -105,6 +105,33 @@ public class EmployeesDao {
 		return list;
 	}
 	
+	// 사원의 최대 갯수를 구하는 메소드
+   public int selectEmpNo(String str) {
+	   int empNo = 0;
+	    Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		String sql = null;
+		if(str.equals("max")) {
+			sql = "SELECT MAX(emp_no) FROM employees";
+		   } else if(str.equals("min")) {
+			   sql = "SELECT MIN(emp_no) FROM employees";
+		   }
+		try {
+			conn = DBHelp.getConncetion();
+			stmt = conn.prepareStatement(sql);
+			rs = stmt.executeQuery();
+			if(rs.next()) {
+				empNo = rs.getInt(1);
+				}
+			} catch(Exception e) {
+				e.printStackTrace();
+			} finally {
+				DBHelp.close(rs, stmt, conn);
+			}
+	   return empNo;
+	  }
+	
 	// 사원의 인구수(성별로 나눴을경우)
 	public List<Map<String, Object>> selectEmployeesCountGroupByGender() {
 		   List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
@@ -165,6 +192,47 @@ public class EmployeesDao {
 				}
 			return list;
 		}
+	   
+	   // employees의 리스트를 페이징 작업해서 보여주는 메서드
+	   public List<Employees> selectEmployeesListByPage(int currentPage, int rowPerPage) {
+		   List<Employees> list = new ArrayList<Employees>();
+		   Connection conn = null;
+		   PreparedStatement stmt = null;
+		   ResultSet rs = null;
+		   
+		   String sql = "SELECT emp_no, birth_date, first_name, last_name, gender, hire_date FROM employees LIMIT ?, ?";
+		   try {
+			   conn = DBHelp.getConncetion();
+			   stmt = conn.prepareStatement(sql);
+			   rs = stmt.executeQuery();
+			   int startRow = (currentPage - 1) * rowPerPage;
+			   stmt.setInt(1, startRow);
+			   stmt.setInt(2, rowPerPage);
+			   while(rs.next()) {
+				   Employees employees = new Employees();
+				   employees.setEmpNo(rs.getInt("emp_no"));
+				   employees.setBirthDate(rs.getString("birth_date"));
+				   employees.setFirstName(rs.getString("first_name"));
+				   employees.setLastName(rs.getString("last_name"));
+				   employees.setGender(rs.getString("gender"));
+				   employees.setHireDate(rs.getString("hire_date"));
+				   list.add(employees);
+			   }
+		   } catch(Exception e) {
+			   e.printStackTrace();
+		   } finally {
+			   DBHelp.close(rs, stmt, conn);
+		   }
+		   return list;
+	   }
+	   
+	   public int selectLastPage(int rowPerPage) {
+		   int lastPage = this.selectEmployeesCount();
+		   if(lastPage % rowPerPage > 0) {
+			   lastPage++;
+		   }
+		   return lastPage;
+	   }
 }
 
 
